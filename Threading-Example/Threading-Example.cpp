d9fbd3f3ab3fb63804a250e8b1472667e839a351
morozov603v1@gmail.com <morozov603v1@gmail.com>
2020-07-01 20:06:57 +0700

    SomeStuff

--- Threading-Example/Threading-Example.cpp
+++ Threading-Example/Threading-Example.cpp
@@ -18,22 +18,32 @@
 
 using namespace std;
 
+string FizzBuzz(int number, int divider, string input) {
+    if (number % divider == 0) {
+        return input;
+    }
+    else {
+        return "";
+    }
+}
+
+inline string FizzBuzz(int number) {   
+    return  to_string(number);
+}
+
 int main()
 {
     auto FB = [](int x) {
         for (int i = 1; i <= x; i++) {
             string output = "";
-            if (i % 3 == 0) { output += "Fizz"; }
-            if (i % 5 == 0) { output += "Buzz"; }
-
-            if (output == "") {
-                string str = to_string(i);
-                output = str;
-            }
+            output += FizzBuzz(i, 3, "Fizz");
+            output += FizzBuzz(i, 5, "Buzz");
+            output += FizzBuzz(i, 7, "Gezz");
+            if (output == "") { output += FizzBuzz(i); }
             cout << output << endl;
         }
     };
-    thread thr(FB, 15);
+    thread thr(FB, 105);
     thr.join();
 
     return 0;
